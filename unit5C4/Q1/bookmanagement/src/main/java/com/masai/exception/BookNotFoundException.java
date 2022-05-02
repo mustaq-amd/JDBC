fe5ac072432d94545e2bb8dcb52d6ce06c2d67dd
Mohammed Mustaq <97461845+mustaq-amd@users.noreply.github.com>
2022-05-02 13:40:26 +0530

    Update BookNotFoundException.java

--- unit5C4/Q1/bookmanagement/src/main/java/com/masai/exception/BookNotFoundException.java
+++ unit5C4/Q1/bookmanagement/src/main/java/com/masai/exception/BookNotFoundException.java
@@ -3,10 +3,10 @@ package com.masai.exception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class BookNotFoundException extends Exception{
 	
-		@Autowired
+		
 		public BookNotFoundException(String message) {
 			super(message);
 		}
